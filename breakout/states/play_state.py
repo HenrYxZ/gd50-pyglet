6a73d1e78e181ccd18fbeb185237aed58011052f
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-01-21 16:10:36 -0300

    feat: breakout v4 bounce with bricks

--- breakout/states/play_state.py
+++ breakout/states/play_state.py
@@ -41,11 +41,53 @@ class PlayState(BaseState):
         if self.ball.collides(self.paddle):
             self.ball.dy = -self.ball.dy
             self.ball.y = self.paddle.y + self.paddle.height + 1
+            # Add angle influence
+            paddle_center = self.paddle.x + self.paddle.width / 2
+            ball_center = self.ball.x + self.ball.width / 2
+            dist_paddle_ball_x = paddle_center - ball_center
+            if ball_center < paddle_center and self.paddle.dx < 0:
+                # ball coming from the left
+                self.ball.dx = (
+                    -BALL_BOUNCE_SPEED -
+                    BALL_BOUNCE_FACTOR * dist_paddle_ball_x
+                )
+            elif ball_center > paddle_center and self.paddle.dx > 0:
+                # ball coming from right
+                self.ball.dx = (
+                    BALL_BOUNCE_SPEED +
+                    BALL_BOUNCE_FACTOR * abs(dist_paddle_ball_x)
+                )
+
             utils.play(sounds[PADDLE_HIT])
         # Handle collision with bricks that are in play
         for brick in filter(lambda l: l.in_play, self.bricks):
             if self.ball.collides(brick):
                 brick.hit()
+                # bounce ball
+                ball_right = self.ball.x + self.ball.width
+                ball_top = self.ball.y + self.ball.height
+                brick_top = brick.y + brick.height
+                brick_right = brick.x + brick.width
+                if ball_right - 2 < brick.x and self.ball.dx > 0:
+                    # left collision
+                    self.ball.dx = -self.ball.dx
+                    self.ball.x = brick.x - self.ball.width
+                elif ball_right + 2 > brick_right and self.ball.dx < 0:
+                    # right collision
+                    self.ball.dx = -self.ball.dx
+                    self.ball.x = brick_right
+                elif ball_top < brick_top:
+                    # top collision
+                    self.ball.dy = -self.ball.dy
+                    self.ball.y = brick_top
+                else:
+                    # bottom collision
+                    self.ball.dy = -self.ball.dy
+                    self.ball.y = brick.y - self.ball.height
+                # Apply small factor for momentum
+                self.ball.dy *= BALL_DY_FACTOR
+                # Only allow collision with one brick
+                break
 
     def render(self):
         for brick in filter(lambda l: l.in_play, self.bricks):
