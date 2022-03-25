328136dce010f077570f61cc49a4baba122d2897
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-03-25 18:12:12 -0300

    feat: match3 tween 0

--- match3/main.py
+++ match3/main.py
@@ -1,44 +1,40 @@
 import pyglet
+from pyglet.text import Label
 
 
 from constants import *
-from timer import SingleTimer, Timer
 
 
 window = pyglet.window.Window(WIDTH, HEIGHT, "Match 3")
-labels = []
-fps_label = pyglet.text.Label(color=COLOR_FPS, font_size=MEDIUM)
+label = Label(x=4*SCALE, y=HEIGHT-4*SCALE, font_size=8)
+fps_label = Label(color=COLOR_FPS, font_size=MEDIUM)
 fps_display = pyglet.window.FPSDisplay(window)
 fps_display.label = fps_label
 
+MOVE_DURATION = 2
+flappy_img = pyglet.image.load("flappy.png")
+timer = 0
+
 
 def update(dt):
-    timer.update(dt)
-    for i, label in enumerate(labels):
-        label.text = f"Timer: {timer.items[i].counter} seconds"
+    global timer
+    if timer < MOVE_DURATION:
+        timer += dt
+        flappy_sprite.x = min(end_x, (timer / MOVE_DURATION) * end_x)
+
+    label.text = f"{timer:.3f}"
 
 
 @window.event
 def on_draw():
     window.clear()
-    for label in labels:
-        label.draw()
+    flappy_sprite.draw()
+    label.draw()
     fps_display.draw()
 
 
 if __name__ == '__main__':
-    intervals = [1, 2, 4, 3, 2]
-    n = len(intervals)
-    timer = Timer()
-    for interval in intervals:
-        timer.schedule(interval)
-    for j in range(n):
-        labels.append(
-            pyglet.text.Label(
-                x=WIDTH/2, y=HEIGHT-(70+j*16)*SCALE,
-                anchor_x='center',
-                anchor_y='center'
-            )
-        )
+    flappy_sprite = pyglet.sprite.Sprite(flappy_img, x=0, y=HEIGHT/2)
+    end_x = WIDTH - flappy_sprite.width
     pyglet.clock.schedule_interval(update, REFRESH_RATE)
     pyglet.app.run()
