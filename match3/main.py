7d8c359a777aaa0fde8c5b46046a7e37433c102c
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-03-25 21:01:23 -0300

    feat: match3 tween 1

--- match3/main.py
+++ match3/main.py
@@ -3,6 +3,7 @@ from pyglet.text import Label
 
 
 from constants import *
+from random import random, uniform, randrange
 
 
 window = pyglet.window.Window(WIDTH, HEIGHT, "Match 3")
@@ -11,30 +12,39 @@ fps_label = Label(color=COLOR_FPS, font_size=MEDIUM)
 fps_display = pyglet.window.FPSDisplay(window)
 fps_display.label = fps_label
 
-MOVE_DURATION = 2
+TIMER_MAX = 10
 flappy_img = pyglet.image.load("flappy.png")
 timer = 0
+batch = pyglet.graphics.Batch()
 
 
 def update(dt):
     global timer
-    if timer < MOVE_DURATION:
+    if timer < TIMER_MAX:
         timer += dt
-        flappy_sprite.x = min(end_x, (timer / MOVE_DURATION) * end_x)
-
+        for b, r in birds:
+            b.x = min(end_x, (timer / r) * end_x)
     label.text = f"{timer:.3f}"
 
 
 @window.event
 def on_draw():
     window.clear()
-    flappy_sprite.draw()
+    batch.draw()
     label.draw()
     fps_display.draw()
 
 
 if __name__ == '__main__':
-    flappy_sprite = pyglet.sprite.Sprite(flappy_img, x=0, y=HEIGHT/2)
-    end_x = WIDTH - flappy_sprite.width
+    end_x = WIDTH - flappy_img.width
+    # Create 1000 birds
+    birds = []
+    num_birds = 1000
+    for i in range(num_birds):
+        x = 0
+        y = randrange(HEIGHT-24*SCALE)
+        rate = uniform(1, TIMER_MAX)
+        bird = pyglet.sprite.Sprite(flappy_img, x=x, y=y, batch=batch)
+        birds.append([bird, rate])
     pyglet.clock.schedule_interval(update, REFRESH_RATE)
     pyglet.app.run()
