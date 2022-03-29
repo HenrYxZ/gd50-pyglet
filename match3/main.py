061d84f8c22416767b6f26b85965db619784d0a9
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-03-29 15:26:57 -0300

    feat: match3 chain 0

--- match3/main.py
+++ match3/main.py
@@ -8,43 +8,53 @@ from timer import Timer
 
 
 window = pyglet.window.Window(WIDTH, HEIGHT, "Match 3")
-label = Label(x=4*SCALE, y=HEIGHT-4*SCALE, font_size=8)
 fps_label = Label(color=COLOR_FPS, font_size=MEDIUM)
 fps_display = pyglet.window.FPSDisplay(window)
 fps_display.label = fps_label
 
-TIMER_MAX = 10
+MOVEMENT_TIME = 2
 flappy_img = pyglet.image.load("flappy.png")
 batch = pyglet.graphics.Batch()
 timer = 0
+current_dest_idx = 0
 
 
 def update(dt):
-    global timer
-    if timer < TIMER_MAX:
-        timer += dt
-        timer_obj.update(dt)
-    label.text = f"{timer:.3f}"
+    global base_x, base_y, current_dest_idx, timer
+    if current_dest_idx == len(destinations):
+        return
+    timer = min(MOVEMENT_TIME, timer + dt)
+    if timer == MOVEMENT_TIME:
+        timer = 0
+        base_x = destinations[current_dest_idx]['x']
+        base_y = destinations[current_dest_idx]['y']
+        current_dest_idx += 1
+
+    else:
+        destination = destinations[current_dest_idx]
+        bird.x = base_x + (destination['x'] - base_x) * (timer / MOVEMENT_TIME)
+        bird.y = base_y + (destination['y'] - base_y) * (timer / MOVEMENT_TIME)
 
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-    label.draw()
     fps_display.draw()
 
 
 if __name__ == '__main__':
     end_x = WIDTH - flappy_img.width
+    end_y = HEIGHT - flappy_img.height
     # Create 1000 birds
-    num_birds = 1000
-    timer_obj = Timer()
-    for i in range(num_birds):
-        x = 0
-        y = randrange(HEIGHT-24*SCALE)
-        rate = uniform(1, TIMER_MAX)
-        bird = pyglet.sprite.Sprite(flappy_img, x=x, y=y, batch=batch)
-        timer_obj.tween(rate, bird, {"x": end_x, "opacity": 255})
+    destinations = [
+        {"x": end_x, "y": end_y},
+        {"x": end_x, "y": 0},
+        {"x": 0, "y": 0},
+        {"x": 0, "y": end_y},
+    ]
+    base_x = 0
+    base_y = end_y
+    bird = pyglet.sprite.Sprite(flappy_img, x=base_x, y=base_y, batch=batch)
     pyglet.clock.schedule_interval(update, REFRESH_RATE)
     pyglet.app.run()
