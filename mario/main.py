607c1b58328278ba4be252d6c52bb294d1a7bea5
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-06-20 11:31:10 -0400

    feat: mario character4 jump

--- mario/main.py
+++ mario/main.py
@@ -23,12 +23,13 @@ map_width = 20
 map_height = 9
 background_color = np.random.random_sample(3)
 camera_scroll = 0
+GROUND_LEVEL = 3
 
 # create tile map
 for j in range(map_height):
     tile_map.append([])
     for i in range(map_width):
-        tile_id = SKY_ID if j < 5 else GROUND_ID
+        tile_id = SKY_ID if j < (map_height - GROUND_LEVEL) else GROUND_ID
         tile_map[j].append(tile_id)
 
 # create sprites
@@ -48,39 +49,62 @@ character_quads = pyglet.image.ImageGrid(character_sheet, 1, 11)
 for img in character_quads:
     img.anchor_x = CHARACTER_WIDTH / 2
 
-# two animations
+# The animations
 idle_animation = Animation([0], 1)
 moving_animation = Animation([9, 10], 0.2)
+jump_animation = Animation([3], 1)
 
 current_animation = idle_animation
 direction = "right"
 
 character_x = WIDTH / 2
-character_y = 4 * TILE_SIZE
+character_y = GROUND_LEVEL * TILE_SIZE
 character_sprite = Sprite(
     character_quads[0],
     x=character_x, y=character_y, batch=batch
 )
+character_dy = 0
+
+
+def on_key_press(symbol, _):
+    global character_dy, current_animation
+    if symbol == key.SPACE:
+        character_dy = JUMP_SPEED
+        current_animation = jump_animation
 
 
 def update(dt):
-    global camera_scroll, character_x, current_animation, direction
+    global camera_scroll, character_x, character_dy, character_y, \
+        current_animation, direction
+
+    # apply velocity to character y
+    character_dy -= GRAVITY
+    character_y += character_dy * dt
+
+    if character_y < GROUND_LEVEL * TILE_SIZE:
+        character_y = GROUND_LEVEL * TILE_SIZE
+        character_dy = 0
+    character_sprite.y = character_y
+
+    current_animation.update(dt)
+
     if keys[key.LEFT]:
         character_sprite.x -= CHARACTER_MOVE_SPEED * dt
         direction = "left"
-        current_animation = moving_animation
+        if character_dy == 0:
+            current_animation = moving_animation
         character_sprite.scale_x = -1
     elif keys[key.RIGHT]:
         character_sprite.x += CHARACTER_MOVE_SPEED * dt
         direction = "right"
-        current_animation = moving_animation
+        if character_dy == 0:
+            current_animation = moving_animation
         character_sprite.scale_x = 1
     else:
         current_animation = idle_animation
         character_sprite.scale_x = 1
 
     camera_scroll = character_sprite.x - WIDTH / 2 - CHARACTER_WIDTH / 2
-    current_animation.update(dt)
 
 
 @window.event
@@ -97,5 +121,6 @@ def on_draw():
 
 
 if __name__ == '__main__':
+    window.push_handlers(on_key_press=on_key_press)
     pyglet.clock.schedule_interval(update, 1 / FPS)
     pyglet.app.run()
